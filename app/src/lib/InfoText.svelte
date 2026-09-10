cc18301012edbb149f803a212cfc6b652781e928
Giacomo Nanni <gn.nanni@gmail.com>
2026-09-10 14:42:21 +0200

    credits

--- app/src/lib/InfoText.svelte
+++ app/src/lib/InfoText.svelte
@@ -5,10 +5,10 @@
       href="https://maind.supsi.ch/master-interaction-design/"
       target="_blank"
       rel="noopener noreferrer">Data Driven Design course</a
-    > at SUPSI's Master of Arts in Interaction Design, led by Antonella and Ginevra,
-    part of 2026's focus on the materialities of AI. It uses a safety-filtered
-    sample of LAION-5B, one of the datasets used to train large text-to-image
-    models.
+    > at SUPSI's Master of Arts in Interaction Design, led by Antonella Autuori
+    and Ginevra Terenghi, part of 2026's focus on the materialities of AI. It
+    uses a safety-filtered sample of LAION-5B, one of the datasets used to
+    train large text-to-image models.
   </p>
   <p class="mb-4 block">
     Click on any image to see its caption and a link back to that original
@@ -19,7 +19,7 @@
       class="underline"
       href="https://giacomo.website"
       target="_blank"
-      rel="noopener noreferrer">Giacomo</a
+      rel="noopener noreferrer">Giacomo Nanni</a
     >.
   </p>
 </div>

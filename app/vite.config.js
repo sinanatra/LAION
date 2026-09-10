bff66e444c2c899d288bce689c5a62f997449c11
Giacomo Nanni <gn.nanni@gmail.com>
2026-09-10 15:27:57 +0200

    fixed assets path

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/LAION/' : '/',
+  base: '/',
   plugins: [tailwindcss(), svelte()],
 })
